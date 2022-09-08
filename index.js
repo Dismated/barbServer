e4ff0ae387fe0e005724ae0129898b89534ffe65
dismated <platakis.eimantas@gmail.com>
2022-09-08 20:26:20 +0100

    uibuild

--- index.js
+++ index.js
@@ -12,34 +12,34 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(cors());
 app.use(express.static("build"));
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
-app.get("/api/products", async (request, response) => {
+app.get("/api/products", async (request, response, next) => {
   try {
     const products = await Product.find({});
     response.json(products);
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 });
 
-app.get("/api/products/:id", (request, response) => {
+app.get("/api/products/:id", async (request, response, next) => {
   try {
-    const product = Product.findById(request.params.id);
+    const product = await Product.findById(request.params.id);
     if (product) {
       response.json(product);
     } else {
       response.status(404).end();
     }
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 });
 
-app.post("/api/products", (request, response) => {
+app.post("/api/products", (request, response, next) => {
   const body = request.body;
 
   if (body === undefined) {
@@ -51,22 +51,57 @@ app.post("/api/products", (request, response) => {
   try {
     const savedProduct = product.save();
     response.json(savedProduct);
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
+});
+app.put("/api/products/:id", async (request, response, next) => {
+  const body = request.body;
+
+  const product = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  try {
+    const updatedProduct = awaitNote.findByIdAndUpdate(
+      request.params.id,
+      product,
+      { new: true }
+    );
+    response.json(updatedProduct);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.delete("/api/products/:id", (request, response) => {
+app.delete("/api/products/:id", (request, response, next) => {
   try {
     Product.findByIdAndRemove(request.params.id);
     response.status(204).end();
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 });
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
